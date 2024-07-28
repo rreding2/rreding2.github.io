2dabb7394e014980ba9278a40605879a0e5649f5
Robert Reding <rreding@lightspeedanalytics.com>
2024-07-27 22:01:41 -0700

    sss

--- time.js
+++ time.js
@@ -9,30 +9,34 @@ const height = 800;
 let lineUS = d3.line();
 let lineCanada = d3.line();
 
-let xScale = d3.scaleLinear().domain([minYear, maxYear]).range(0, width);
-let yScale = d3.scaleLinear().domain([0, 30]).range(height, 0);
-
-let us;
-let canada;
-
-let lineScale = d3
-  .line()
-  .x(function (d) {
-    return xScale(d.year);
-  })
-  .y(function (d) {
-    return yScale(d.suicides);
-  });
+let xScale = d3.scaleLinear().domain([minYear, maxYear]).range([0, width]);
+let yScale = d3.scaleLinear().domain([0, 200]).range([height, 0]);
 
 function loadData() {
-  let svg = d3
-    .select('#graph')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .style('background', 'pink');
+  console.log(xScale(2000));
+  console.log(yScale(200));
 
-  d3.csv('us.csv').then(function (data) {
-    svg.append('path').attr({ d: lineScale(data) });
+  let svg = d3.select('#graph').append('svg');
+  svg.attr('width', width).attr('height', height).style('background', 'pink');
+
+  d3.csv('us.csv', function (data) {
+    console.log(data);
+
+    let lineScale = d3
+      .line()
+      .x(function (d, i) {
+        return xScale(d.year);
+      })
+      .y(function (d) {
+        return yScale(d.suicides);
+      });
+
+    svg
+      .append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', 'black')
+      .attr('stroke-width', '5')
+      .attr('d', lineScale());
   });
 }
